b5681e0ed4581e6b65d46c6a1dcd8d7afbd52325
Aurink Grellet <aurink.grellet@arietis.Dev>
2023-07-17 15:06:48 +0200

    Add authenticator

---
+++ src/Controller/CarsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CarsController extends AbstractController
+{
+    #[Route('/cars', name: 'cars_show')]
+    public function index(): Response
+    //TODO
+    {
+        return $this->render('cars/index.html.twig', [
+            'controller_name' => 'CarsController',
+        ]);
+    }
+
+    #[Route('/cars/{id}', name: ('cars_showone'))]
+    public function showOne(): Response
+    //TODO
+    {
+        return $this->render('cars/index.html.twig', [
+            'controller_name' => 'CarsController',
+        ]);
+    }
+}
